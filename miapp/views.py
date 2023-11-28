701109b9616c9ae491b2a6e03241974a8b14407d
Sofaxw <sofia.jaraa@usm.cl>
2023-11-27 22:31:51 -0300

    tabla

--- miapp/views.py
+++ miapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Evento
 
 def home(request):
 
+    evento = Evento.objects.all()
+
     data = {
         'title': 'Inicio',
+        'evento': evento,
     }
 
     return render(request, 'miapp/base.html', data)
