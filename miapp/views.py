24b0febf2a1fa5b6d46f9af6660458c09baa4b0e
Y0yin <alonso.sanchez@usm.cl>
2023-11-27 21:01:04 -0300

    Soy alan turing

--- miapp/views.py
+++ miapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'miapp/base.html', name='base')
+
+    data = {
+        'title': 'Inicio',
+    }
+
+    return render(request, 'templates/base.html', data)
